847a904e8dd289b3e993b21a2a537fde335a94b8
Jef Roosens <roosensjef@gmail.com>
2022-06-23 20:43:18 +0200

    feat: start of ContainerCreate struct

--- types/container.v
+++ types/container.v
@@ -1,6 +1,7 @@
 module types
 
 pub struct Port {
+pub:
 	ip           string [json: IP]
 	private_port u16    [json: PrivatePort]
 	public_port  u16    [json: PublicPort]
@@ -8,16 +9,19 @@ pub struct Port {
 }
 
 pub struct HostConfig {
+pub:
 	network_mode string [json: NetworkMode]
 }
 
 pub struct EndpointIpamConfig {
+pub:
 	ipv4_address   string   [json: IPv4Address]
 	ipv6_address   string   [json: IPv6Address]
 	link_local_ips []string [json: LinkLocalIPs]
 }
 
 pub struct EndpointSettings {
+pub:
 	ipam_config            EndpointIpamConfig [json: IPAMConfig]
 	links                  []string           [json: Links]
 	aliases                []string           [json: Aliases]
@@ -34,10 +38,12 @@ pub struct EndpointSettings {
 }
 
 pub struct NetworkSettings {
+pub:
 	networks map[string]EndpointSettings [json: Networks]
 }
 
 pub struct MountPoint {
+pub:
 	type_       string [json: Type]
 	name        string [json: Name]
 	source      string [json: Source]
@@ -49,6 +55,7 @@ pub struct MountPoint {
 }
 
 pub struct ContainerListItem {
+pub:
 	id               string            [json: Id]
 	names            []string          [json: Names]
 	image            string            [json: Image]
@@ -64,4 +71,41 @@ pub struct ContainerListItem {
 	host_config      HostConfig        [json: HostConfig]
 	network_settings NetworkSettings   [json: NetworkSettings]
 	mounts           []MountPoint      [json: Mounts]
+}
+
+pub struct HealthConfig {
+pub:
+    test []string [json: Test]
+    interval int [json: Interval]
+    timeout int [json: Timeout]
+    retries int [json: Retries]
+    start_period int [json: StartPeriod]
+}
+
+pub struct ContainerCreate {
+pub:
+    hostname string [json: Hostname]
+    domain_name string [json: Domainname]
+    user string [json: User]
+    attach_stdin bool [json: AttachStdin]
+    attach_stdout bool [json: AttachStderr] = true
+    // ExposedPorts
+    tty bool [json: Tty]
+    open_stdin bool [json: OpenStdin]
+    stdin_once bool [json: StdinOnce]
+    env []string [json: Env]
+    cmd []string [json: Cmd]
+    healthcheck HealthConfig [json: Healthcheck]
+    args_escaped bool [json: ArgsEscaped]
+    image string [json: Image]
+    // Volumes
+    working_dir string [json: WorkingDir]
+    entrypoint []string [json: Entrypoint]
+    network_disabled bool [json: NetworkDisabled]
+    mac_address string [json: MacAddress]
+    on_build []string [json: OnBuild]
+    labels map[string]string [json: Labels]
+    stop_signal string [json: StopSignal]
+    stop_timeout int [json: StopTimeout]
+    shell []string [json: Shell]
 }
